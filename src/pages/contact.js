bf07603a608d74ce0bce4057873dd725387b5fae
Ali Klein <alexandraeklein@yahoo.com>
2020-08-02 09:51:31 -0700

    Form submission with axios

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,54 @@
 import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
+import axios from "axios"
 
-const contact = () => {
+const Contact = () => {
+  const [serverState, setServerState] = React.useState({
+    submitting: false,
+    status: null,
+  })
+  const handleServerResponse = (ok, msg, form) => {
+    setServerState({
+      submitting: false,
+      status: { ok, msg },
+    })
+    if (ok) {
+      form.reset()
+    }
+  }
+  const handleOnSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    setServerState({ submitting: true })
+    axios({
+      method: "post",
+      url: "https://formspree.io/xjvalgzq",
+      data: new FormData(form),
+    })
+      .then(r => {
+        handleServerResponse(
+          true,
+          "Thank you! Your message has been sent!",
+          form
+        )
+      })
+      .catch(r => {
+        handleServerResponse(
+          false,
+          "Something went wrong. Please try again.",
+          form
+        )
+      })
+  }
   return (
     <Layout>
       <SEO title="Contact" description="Contact Me" />
       <section className="contact-page">
         <article className="contact-form">
           <h3>Get in touch</h3>
-          <form action="https://formspree.io/xjvalgzq" method="POST">
+
+          <form onSubmit={handleOnSubmit}>
             <div className="form-group">
               <input
                 type="text"
@@ -22,17 +61,30 @@ const contact = () => {
                 name="email"
                 placeholder="email"
                 className="form-control"
+                required
               />
               <textarea
                 name="message"
                 rows="5"
                 placeholder="message"
                 className="form-control"
-              ></textarea>
+              />
+              <button
+                className="btn submit-btn"
+                type="submit"
+                disabled={serverState.submitting}
+              >
+                Submit
+              </button>
+              {serverState.status && (
+                <div className="form-message">
+                  <p className={!serverState.status.ok ? "error" : "success"}>
+                    {console.log({ serverState })}
+                    {serverState.status.msg}
+                  </p>
+                </div>
+              )}
             </div>
-            <button className="btn submit-btn" type="submit">
-              Submit
-            </button>
           </form>
         </article>
       </section>
@@ -40,4 +92,4 @@ const contact = () => {
   )
 }
 
-export default contact
+export default Contact
